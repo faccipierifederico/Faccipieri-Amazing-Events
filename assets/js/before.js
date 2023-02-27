c4793a2250949e0fbb7352cf274f33ccc717b927
Federico Faccipieri <federicofaccipieri@gmail.com>
2023-02-27 13:17:30 -0300

    Versión 6.2: agregación de JavaScript

--- assets/js/before.js
+++ assets/js/before.js
@@ -13,9 +13,9 @@ let fragment = document.createDocumentFragment();
 
 for (let event of before) {
     let div = document.createElement('div')
-    div.classList.add('border', 'w-50', 'new-div', 'd-flex', 'flex-column')
+    div.classList.add('border', 'new-div', 'd-flex', 'flex-column', 'col-10', 'col-sm-10', 'col-md-4', 'col-lg-3')
     div.innerHTML = `<h2 class="text-center">${event.name}</h2>`
-    div.innerHTML += `<img src="${event.image}" width="100%"></img>`
+    div.innerHTML += `<img class="h-75" src="${event.image}" width="100%"></img>`
     div.innerHTML += `<p class="new-div">Description of the event: ${event.description}</p>`
     div.innerHTML += `<p class="new-div">Event price: ${event.price} dollars</p>`
     fragment.appendChild(div)
